3d4e90355c72b5826f60f4b4344bf4bb8a51e859
reydstry <kbrzraihan@gmail.com>
2025-03-16 21:29:08 +0800

    coba men

---
+++ src/components/Slider.js
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Slider from '@mui/material/Slider';
+import MuiInput from '@mui/material/Input';
+import { Typography } from '@mui/material';
+
+const Input = styled(MuiInput)`
+  width: 42px;
+  color: white;
+`;
+
+export default function ContinuousSlider(props) {
+  const value = props.value
+  const setValue = props.setValue
+  const minValue = props.minValue
+  const maxValue = props.maxValue
+  const label = props.label
+
+  const handleSliderChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleInputChange = (event) => {
+    setValue(event.target.value === '' ? '' : Number(event.target.value));
+  };
+
+  const handleBlur = () => {
+    if (value < minValue) {
+      setValue(minValue);
+    } else if (value > maxValue) {
+      setValue(maxValue);
+    }
+  };
+
+  return (
+    <Box sx={{ minWidth: 300 }}>
+      <Grid container spacing={2} alignItems="center">
+        <Grid item xs>
+          <Slider
+            value={typeof value === 'number' ? value : 0}
+            onChange={handleSliderChange}
+            min={minValue}
+            max={maxValue}
+            aria-labelledby="input-slider"
+          />
+        </Grid>
+        <Grid item>
+          <Input
+            color='primary'
+            value={value}
+            size="small"
+            onChange={handleInputChange}
+            onBlur={handleBlur}
+            inputProps={{
+              step: 1,
+              min: minValue,
+              max: maxValue,
+              type: 'number',
+              'aria-labelledby': 'input-slider',
+            }}
+          />
+        </Grid>
+        <Grid>
+          <Typography
+            color={'white'}
+          >
+            {label}
+          </Typography>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
